491773244967d2ca819d270d01f570600934a719
LAW933 <bongalonlawgen99@gmail.com>
2024-09-25 23:54:12 +0800

    Create Bongalon.py

---
+++ Midterm/TASK 1/Bongalon.py
@@ -0,0 +1,10 @@
+# checks if it is positive, negative or zero.
+num = float(input("enter number:"))
+
+if num > 0: print("positive.")
+
+elif num < 0: print("negative.")
+
+else: 
+    print("zero.")
+    
